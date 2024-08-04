8cf1b7bb9efe390c0236950decb3e2f5d1cc3c6a
fairuzald <fairuzy210@gmail.com>
2024-08-04 18:24:05 +0700

    feat: update port deploy

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('docs', app, document);
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
